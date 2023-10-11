0005642bb3d1bf3efc293419db1364dfb5ee3e8d
OmitNomis <simon98190@gmail.com>
2023-10-11 11:55:17 +0545

    configure Next Themes for dark mode

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,12 +1,16 @@
+'use client'
 import { NextUIProvider } from "@nextui-org/react";
 import { FC, PropsWithChildren } from 'react'
+import { ThemeProvider } from "next-themes";
 
 export const AppProviders: FC<PropsWithChildren> = ({
     children
 }) => {
     return (
         <NextUIProvider>
-            {children}
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+                {children}
+            </ThemeProvider>
         </NextUIProvider>
     )
 };
