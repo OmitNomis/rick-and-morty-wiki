01ea153c7dede363a35496a7298fe8bcbede9338
OmitNomis <simon98190@gmail.com>
2023-10-12 12:04:10 +0545

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen dark:bg-gray-900 bg-teal-50`}>
         <AppProviders>
-          {children}
+          <main>
+            {children}
+          </main>
         </AppProviders>
       </body>
     </html>
