ddad4feca28ade6362fd79c22cebd6feb2f1423e
OmitNomis <simon98190@gmail.com>
2023-10-13 12:43:39 +0545

    suppress hydration warning for darktheme class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} flex flex-col min-h-screen dark:bg-gray-900 bg-teal-50`}>
         <AppProviders>
