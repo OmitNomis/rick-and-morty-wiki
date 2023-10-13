66fc54c76e6b188d847f9eccd025a51f784bf016
OmitNomis <simon98190@gmail.com>
2023-10-13 12:39:23 +0545

    config header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
-import { AppProviders } from '@/providers/AppProviders'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { AppProviders } from "@/providers/AppProviders";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Header } from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Rick and Morty Wiki',
-  description: 'A wiki for Rick and Morty Characters, Locations, and Episodes',
-}
+  title: "Rick and Morty Wiki",
+  description: "A wiki for Rick and Morty Characters, Locations, and Episodes",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen dark:bg-gray-900 bg-teal-50`}>
+      <body
+        className={`${inter.className} flex flex-col min-h-screen dark:bg-gray-900 bg-teal-50`}>
         <AppProviders>
-          <main>
-            {children}
-          </main>
+          <Header />
+          <main>{children}</main>
         </AppProviders>
       </body>
     </html>
-  )
+  );
 }
