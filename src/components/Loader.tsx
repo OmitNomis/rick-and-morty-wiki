9a2f2216383023ac4fc7353c8531304140016fed
OmitNomis <simon98190@gmail.com>
2023-10-28 23:33:05 +0545

    lint fixes

---
+++ src/components/Loader.tsx
@@ -0,0 +1,28 @@
+import { FC, useMemo } from "react";
+import { MaxWidthComponent } from "./ui/MaxWidthComponent";
+
+const animationsList = [
+  "https://media.giphy.com/media/dz6NpfESnqqcnJH1MY/giphy.gif",
+  "https://media.giphy.com/media/g04poZxA1nAyTs9DQY/giphy.gif",
+  "https://media.giphy.com/media/QyKKSidc5rd0Y1Iw3z/giphy.gif",
+  "https://media.giphy.com/media/l378w2NKt3URm6w5W/giphy.gif",
+];
+
+export const Loader: FC = () => {
+  const randomNumber = Math.floor(Math.random() * animationsList.length);
+
+  return useMemo(() => {
+    const generateAnimation = (list: string[], id: number): string => {
+      return list[id];
+    };
+    return (
+      <MaxWidthComponent className="flex justify-center items-center">
+        <img
+          height="300"
+          src={generateAnimation(animationsList, randomNumber)}
+          alt="loader"
+        />
+      </MaxWidthComponent>
+    );
+  }, []);
+};
