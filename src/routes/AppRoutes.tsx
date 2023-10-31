9adf4a92fd85d809a7a79b87722095e71ee45f96
OmitNomis <simon98190@gmail.com>
2023-10-31 17:02:37 +0545

    add a component for Single Character Profile

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./Layout";
 import { Characters, Locations, Episodes } from "@/screens";
+import { CharacterProfile } from "@/screens/CharacterProfile/CharacterProfile";
 
 export const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Navigate to={"/characters"} />} />
           <Route path="/characters" element={<Characters />} />
+          <Route path="/characters/:id" element={<CharacterProfile />} />
           <Route path="/Locations" element={<Locations />} />
           <Route path="/Episodes" element={<Episodes />} />
           <Route path="*" element={<div>Not Found</div>} />
