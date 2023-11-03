d0815a0ea3b88297c0504c8467b4eea6fdf8da8d
OmitNomis <simon98190@gmail.com>
2023-11-03 22:19:22 +0545

    locations list UI

--- src/screens/Locations/Locations.tsx
+++ src/screens/Locations/Locations.tsx
@@ -1,3 +1,50 @@
-export const Locations = () => {
-  return <div>Locations</div>;
+import { FC, useState } from "react";
+import { useFetchLocations } from "@/hooks/useFetchLocations";
+import { Loader } from "@/components/Loader";
+import { CustomPagination } from "@/components/ui/CustomPagination";
+import { LocationCard } from "@/components/LocationCard";
+import { LocationsResult } from "@/types/Location.types";
+
+const initialFilters = {
+  name: "",
+  type: "",
+  dimension: "",
+};
+export const Locations: FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  // todo: add filters with form
+  // const [filters, setFilters] = useState({});
+  const { locations, error, loading, totalItems } = useFetchLocations(
+    currentPage,
+    initialFilters
+  );
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <>ERr</>;
+  }
+
+  return (
+    <section className="flex flex-col gap-5">
+      <div>
+        <h1 className="text-3xl sm:text-4xl font-extrabold">Locations</h1>
+      </div>
+      <div className="mt-5 w-full">
+        {locations.map((location: LocationsResult) => {
+          return <LocationCard key={location.id} location={location} />;
+        })}
+      </div>
+      <div>
+        <CustomPagination
+          currentPage={currentPage}
+          totalItems={totalItems}
+          showNextPrevButtons
+          handleChange={setCurrentPage}
+        />
+      </div>
+    </section>
+  );
 };
