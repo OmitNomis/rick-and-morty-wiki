75d44b6a7532b0b27ee0adb4fed5ec63b0a12f83
OmitNomis <simon98190@gmail.com>
2023-11-03 22:17:38 +0545

    change types

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { CharacterResults } from "@/screens/Characrers/Characters.types";
+import { CharacterResults } from "@/types/Characters.types";
 import { CharacterStatusTag } from "./CharacterStatusTag";
 import { Link } from "react-router-dom";
 
@@ -25,20 +25,24 @@ export const CharacterCard: FC<CharacterCardProps> = ({ character }) => {
       <div className="flex flex-col p-5 gap-5 w-full sm:w-[60%]">
         <div>
           <Link to={id}>
-            <div className="text-xl font-bold sm:text-2xl hover:text-primary">
+            <h3 className="text-xl font-bold sm:text-2xl hover:text-primary">
               {name}
-            </div>
+            </h3>
           </Link>
           <CharacterStatusTag status={status} species={species} type={type} />
         </div>
-        <div>
-          <div className="text-gray-400">Last known location:</div>
-          <div>{location.name}</div>
-        </div>
-        <div>
-          <div className="text-gray-400">Character Origin:</div>
-          <div>{origin.name}</div>
-        </div>
+        {location && (
+          <div>
+            <div className="text-gray-400">Last known location:</div>
+            <div>{location.name}</div>
+          </div>
+        )}
+        {origin && (
+          <div>
+            <div className="text-gray-400">Character Origin:</div>
+            <div>{origin.name}</div>
+          </div>
+        )}
       </div>
     </article>
   );
