2fc41d7c6f59dc971921dcea5889f89d6fe224bb
OmitNomis <simon98190@gmail.com>
2023-11-04 13:12:52 +0545

    preload images test

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,8 +1,18 @@
 import { useTheme } from "@/providers/ThemeProvider";
 import { LightButton, DarkButton } from "@/assets/images";
+import { useEffect } from "react";
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    // preload images
+    const images = [LightButton, DarkButton];
+    images.map((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+  }, []);
 
   const handleThemeSwitch = () => {
     if (theme === "light") {
